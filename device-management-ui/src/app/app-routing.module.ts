349ee5473105528e12f1d7ee9ca91c2dd9f918ec
Sylvio Oliveira <sylvio.romero@gmail.com>
2022-02-27 15:48:37 -0400

    Projeto: device-management-ui Correção no arquivo de configuração de rota (eliminação de rotas desnecessárias).

--- device-management-ui/src/app/app-routing.module.ts
+++ device-management-ui/src/app/app-routing.module.ts
@@ -1,15 +1,11 @@
-import { CategoryCreateComponent } from './categories/category-create/category-create.component';
 import { CategoryListComponent } from './categories/category-list/category-list.component';
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DeviceListComponent } from './devices/device-list/device-list.component';
-import { DeviceCreateComponent } from './devices/device-create/device-create.component';
 
 const routes: Routes = [
   { path: '', component: CategoryListComponent },
-  { path: 'createCategory', component: CategoryCreateComponent },
   { path: 'devices', component: DeviceListComponent },
-  { path: 'createDevice', component: DeviceCreateComponent }
 ];
 
 @NgModule({
